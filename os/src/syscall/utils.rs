b8eca16889379847197eb0bd4d769cd67cec35a9
Pneuma <luzimo2665573906@163.com>
2026-04-28 19:36:38 +0800

    实现了基于WaitQueue的io阻塞等待功能

--- os/src/syscall/utils.rs
+++ os/src/syscall/utils.rs
@@ -1,7 +1,9 @@
 // 放在 net/utils.rs 或 syscall/net.rs 顶部
 use crate::net::config::NET_INTERFACE;
-use crate::task::{current_task, suspend_current_and_run_next};
+use crate::task::{WaitQueue, block_current_and_run_next_with_lock, current_task, suspend_current_and_run_next};
 use crate::utils::error::SyscallErr;
+use alloc::sync::Arc;
+use spin::Mutex;
 
 /// 通用的阻塞 I/O 等待循环核心（与具体设备无关）。
 /// `f` 返回 isize: >=0 表示成功字节数，<0 表示 -errno（as_errno_ret 编码）。
@@ -23,6 +25,54 @@ pub fn wait_io_core(mut f: impl FnMut() -> isize, nonblock: bool) -> isize {
                 }
                 inner.refresh_real_timer();
                 drop(inner);
+            }
+            v => return v,
+        }
+    }
+}
+
+// 加入等待队列的阻塞唤醒
+// wait_queue: 传入的等待队列
+// cond：唤醒条件
+pub fn wait_io_core_with_queue(
+    mut f: impl FnMut() -> isize,
+    nonblock: bool,
+    wait_queue: &Mutex<WaitQueue>,
+    mut cond: impl FnMut() -> bool
+    ) -> isize 
+{
+    loop {
+        match f() {
+            v if v >= 0 => return v,
+            v if v == -(SyscallErr::EAGAIN as isize) => {
+                if nonblock {
+                    return v;
+                }
+                let task = current_task().unwrap();
+                {
+                    let inner = task.acquire_inner_lock();
+                    if !inner.sigpending.difference(inner.sigmask).is_empty() {
+                        return -(SyscallErr::EINTR as isize);
+                    }
+                }
+                let mut wait = wait_queue.lock();
+                wait.prepare_to_wait(Arc::downgrade(&task));
+                // 如果已经满足条件，就不睡了，回到loop重试f()
+                if cond() {
+                    wait.finish_wait(&task);
+                    continue;
+                }
+                drop(task);
+                block_current_and_run_next_with_lock(wait);
+                // 从wake_at_most()回来
+                let task = current_task().unwrap();
+                //结束等待
+                wait_queue.lock().finish_wait(&task);
+                let mut inner = task.acquire_inner_lock();
+                if !inner.sigpending.difference(inner.sigmask).is_empty() {
+                    return -(SyscallErr::EINTR as isize);
+                }
+                inner.refresh_real_timer();
             }
             v => return v,
         }
