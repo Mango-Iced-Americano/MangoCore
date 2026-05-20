b78fc277fb8c24455ad8348e3d33d95e2da53850
Panpeach <panpeach@126.com>
2026-05-20 15:35:30 +0800

    重新跑了一版ltp的配置

--- scripts/run_full_test.py
+++ scripts/run_full_test.py
@@ -29,7 +29,7 @@ TESTRESULT_DIR = os.path.join(PROJECT_ROOT, "testresult")
 JUDGE_DIR = os.path.join(PROJECT_ROOT, "judge")
 FS_IMG_DIR = os.path.join(PROJECT_ROOT, "fs-img-dir")
 
-QEMU_TIMEOUT = 600  # 10 分钟
+QEMU_TIMEOUT = int(os.environ.get("QEMU_TIMEOUT", "600"))  # 默认 10 分钟
 
 # ANSI colors
 GREEN = "\033[92m"
